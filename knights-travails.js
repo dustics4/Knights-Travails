7ad5cd017223f1f06acb3caf4563064251b47742
dustics4 <konrad.babowicz21@gmail.com>
2024-09-06 12:34:27 +0100

    Set up directions

--- knights-travails.js
+++ knights-travails.js
@@ -1,3 +1,10 @@
 function knightMoves(start, target){
-    
+    let directions = [
+        [2,1], [1,2], [-1,2], [-2, 1],
+        [-2,-1] , [-1,-2], [1, -2], [2,1]
+    ]
+
+    console.log(directions);
 }
+
+knightMoves([0,0],[7,7]);
