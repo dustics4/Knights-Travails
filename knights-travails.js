4eff14ef7fadc6c29a83f44a6d0e592120eb1f95
dustics4 <konrad.babowicz21@gmail.com>
2024-09-06 12:35:46 +0100

    isvalid function creation

--- knights-travails.js
+++ knights-travails.js
@@ -1,10 +1,14 @@
 function knightMoves(start, target){
-    let directions = [
+    let directions = [ //lists all possible moves for the knight in chess
         [2,1], [1,2], [-1,2], [-2, 1],
         [-2,-1] , [-1,-2], [1, -2], [2,1]
     ]
 
     console.log(directions);
+
+    function isValidPosition(x,y){ //Checks if a possition is within the chessboard boundries
+
+    }
 }
 
 knightMoves([0,0],[7,7]);
