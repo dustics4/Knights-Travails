cf8a29b35eca9372800eadb7499908c79731eccd
Konrad <konrad.babowicz21@gmail.com>
2024-09-07 17:37:25 +0100

    created isValidPosition function. Checked if working

--- knights-travails.js
+++ knights-travails.js
@@ -7,8 +7,11 @@ function knightMoves(start, target){
     console.log(directions);
 
     function isValidPosition(x,y){ //Checks if a possition is within the chessboard boundries
-
+        return ( x >= 0 && x <= 8 && y >= 0 && y <= 8);
     }
+
+    console.log(isValidPosition(0,0));    
+    console.log(isValidPosition(9,1));  
 }
 
 knightMoves([0,0],[7,7]);
